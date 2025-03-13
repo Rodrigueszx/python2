ee645a40d6f7b704f4b4d2ee341180cd764d17df
Rodriguesxz <slrodriguesjrdev@gmail.com>
2025-03-13 20:25:23 -0300

    adicionado aula

---
+++ Python_/5-operadores.py
@@ -0,0 +1,23 @@
+# operadores aritmeticos
+
+num1 = int(input("Digite o primeiro número:\n"))
+num2 = int(input("Digite o segundo número:\n"))
+
+# operadores
+
+sum = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+exp = num1 ** num2
+
+print('=========================')
+print(sum)
+print(sub)
+print(mult)
+print(div)
+print(div)
+print(exp)
+
+print(f'Potencia do numero {num1} por {num2} é {exp}')
