6f062a054d96f80a1b466f28c96e618ec69da02a
Junior Rodrigues <slrodriguesjrdev@gmail.com>
2025-03-14 03:41:17 -0300

    novos arquivos

---
+++ Python_/6-strings.py
@@ -0,0 +1,16 @@
+movieName = 'Top Gun'
+movieName2 = 'top Gun'
+
+print(movieName == movieName2) # Case sensitive
+
+movieDescription = '''Top Gun' is a 1986 American action drama film directed by Tony Scott, and produced by Don Simpson and Jerry Bruckheimer, in association with Paramount Pictures. The screenplay was written by Jim Cash and Jack Epps Jr., and was inspired by an article titled "Top Guns" published in California magazine three years earlier.'''
+
+print(movieName)
+#1-multiplicação de strings
+line = "="
+print(line*112)
+print(movieDescription)
+
+#2 - Procurar uma palavra em uma string
+print("top" in movieName)
+print("ação" in movieName)
