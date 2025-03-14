8665bdee39edaae94b9c7767db0b14e39a803d9e
Junior Rodrigues <slrodriguesjrdev@gmail.com>
2025-03-14 02:27:21 -0300

    adc new file

--- Python_/5-operadores.py
+++ Python_/5-operadores.py
@@ -21,3 +21,24 @@ print(div)
 print(exp)
 
 print(f'Potencia do numero {num1} por {num2} é {exp}')
+print(f'Resto da divisão de {num1} por {num2} é: {mod}')
+
+#comparação
+
+bigger = num1 > num2
+smaller = num1 < num2 
+equal = num1 == num2
+different = num1 != num2 
+biggerEqual = num1 >= num2
+smallerEqual = num1 <= num2
+
+print(f'Os números {num1} e {num2} são iguais? {equal}')
+print(f'O número {num1} é maior ou igual a {num2}? {biggerEqual}')
+
+
+#atribuição
+
+num1 += 1 #num1 + 1
+num1 -= 1 #num1 - 1
+num1 *= 1 #num1 * 1
+num1 /= 1 #num1 / 1
